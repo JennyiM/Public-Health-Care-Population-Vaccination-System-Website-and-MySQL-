1eadc19e1017d646c48a0bec6bc7b1141ecdf992
WuyangP <pengwuyang2years@gmail.com>
2021-11-12 17:51:14 -0500

    fix delete person index

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
             if(isset($_GET['delete_person'])){
           
           
-                include(TEMPLATE . "/delete_person");
+                include(TEMPLATE . "/delete_person.php");
         
         
             }
