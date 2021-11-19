60ecf4c6a71f003b34cb7261d8d3f05928a96c28
Guo0606 <74224294+Guo0606@users.noreply.github.com>
2021-11-18 22:17:47 -0500

    Update perform_vaccine.php

--- resources/templates/perform_vaccine.php
+++ resources/templates/perform_vaccine.php
@@ -14,12 +14,12 @@
             <div class="item-bar col-lg-3 col-md-6 col-sm-12">
             </div>
             <div class="item-bar col-lg-3 col-md-6 col-sm-12">
-                <a class="nav-link" href="index.php?reports"><i class="far fa-calendar-check"></i>
+                <a class="nav-link" href="index.php?perform_vaccine_withapm"><i class="far fa-calendar-check"></i>
                 <p>With appointment</p>
                 </a>
             </div>
             <div class="item-bar col-lg-3 col-md-6 col-sm-12">
-                <a class="nav-link" href="index.php?Facility_information"><i class="fas fa-walking"></i> 
+                <a class="nav-link" href="index.php?perform_vaccine_findperson"><i class="fas fa-walking"></i>  
                 <p>Without appointment</p>
                 </a>
             </div>
