babcdf1845c763d54ff94a105e9c87e50734f795
YiyiYao1996 <49949346+YiyiYao1996@users.noreply.github.com>
2021-11-21 14:29:19 -0500

    Add files via upload
    Q13: Display nurse
    Q18: Display nurse performed

---
+++ resources/templates/display_nurse.php
@@ -0,0 +1,87 @@
+<?php 
+function display_nurse($d,$f){
+  connect();
+  $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+  $date = $d;
+  $facilityID = $f;
+
+$query = "SELECT assigned.employee_ID, person.firstName, person.lastName, person.email, assigned.hourly_rate
+FROM assigned, person, public_worker
+WHERE assigned.SSN = public_worker.SSN
+			AND public_worker.personID = person.personID
+			AND public_worker.title = 'Nurse'
+			AND assigned.facilityID = $f
+			AND '$d' NOT BETWEEN assigned.startDate AND assigned.endDate
+ORDER BY assigned.hourly_rate ASC;";
+
+  if($result = $mysqli-> query($query)){
+    while ($row = $result->fetch_assoc()){
+      $employee_ID = $row["employee_ID"];
+      $firstName = $row["firstName"];
+      $lastName = $row["lastName"];
+      $email = $row["email"];
+      $hourly_rate = $row["hourly_rate"];
+
+      $display_nurse = <<<DELIMETER
+      <tr role="row">
+          <td row="cell">{$employee_ID}</td>
+          <td row="cell">{$firstName}</td>
+          <td row="cell">{$lastName}</td>
+          <td row="cell">{$email}</td>
+          <td row="cell">{$hourly_rate}</td>
+      </tr> 
+      DELIMETER;
+      echo $display_nurse;
+    }
+    $result->free();
+  }
+}
+?>
+<div class="main col-lg-9 col-md-9 py-3 flex-grow-1 ">
+            <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Display Nurse</li>
+                </ol>
+            </nav>
+            <form class="user_info" action="" method="post" enctype="multipart/form-data">
+            <div class="form-row">
+                <div class="col-lg-4 mb-3">
+                    <label for="date">Date</label>
+                    <input type="text" name="date" placeholder="(YYYY-MM-DD)"  required>
+                </div>
+
+                <div class="col-lg-4 mb-3">
+                    <label for="date">Facility ID</label>
+                    <input type="text" name="facilityID" placeholder="facility ID"  required>
+                </div>
+                     <!--    confirm button-->
+                <div class="confirm_button col-lg-4 mb-3">
+                      <input type="submit" name="search" class="btn btn-dark pull-right" value="Search" >
+                </div>
+            </div>
+            <h6 class="bg-success"><?php display_message(); ?></h6>
+        </form>
+<table class="table table-hover" role="table">
+                <thead role="rowgroup">
+                <tr role="row">
+                    <th scope="col" role="columnheader">Employee ID</th>
+                    <th scope="col" role="columnheader">First Name</th>
+                    <th scope="col" role="columnheader">Last Name</th>
+                    <th scope="col" role="columnheader">Email</th>
+                    <th scope="col" role="columnheader">Hourly Rate</th>
+                </tr>
+                </thead>
+                <tbody role="rowgroup">
+                <?php 
+                $date = "";
+                $facilityID = 0;
+                  if (isset($_POST['search'])) {
+                      $date = $_POST['date'];
+                      $facilityID = (int)$_POST['facilityID'];
+                      display_nurse($date,$facilityID); 
+                  }
+                ?>
+                </tbody>
+            </table>
+</div>
---
+++ resources/templates/display_nurse_performed.php
@@ -0,0 +1,72 @@
+<?php 
+function display_nurse_performed(){
+  connect();
+  $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+
+$query = "SELECT person.firstName, person.lastName, person.phone, COUNT(vaccination.employee_ID) AS 'Number_of_Dose'
+FROM person, public_worker, vaccination,assigned
+WHERE public_worker.title = 'Nurse'
+		AND public_worker.personID = person.personID
+		AND vaccination.employee_ID = assigned.employee_ID
+		AND vaccination.facilityID = assigned.facilityID
+		AND assigned.SSN = public_worker.SSN
+GROUP BY person.personID
+HAVING Number_of_Dose>=20;";
+
+    if($result = $mysqli-> query($query)){
+    while ($row = $result->fetch_assoc()){
+      $firstName = $row["firstName"];
+      $lastName = $row["lastName"];
+      $phone = $row["phone"];
+      $Number_of_Dose = $row["Number_of_Dose"];
+
+      $display_nurse_performed = <<<DELIMETER
+      <tr role="row">
+          <td row="cell">{$firstName}</td>
+          <td row="cell">{$lastName}</td>
+          <td row="cell">{$phone}</td>
+          <td row="cell">{$Number_of_Dose}</td>
+      </tr> 
+      DELIMETER;
+      echo $display_nurse_performed;
+    }
+    $result->free();
+  }
+}
+?>
+<div class="main col-lg-9 col-md-9 py-3 flex-grow-1 ">
+            <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Display Nurse Performed</li>
+                </ol>
+            </nav>
+            <form class="user_info" action="" method="post" enctype="multipart/form-data">
+            <div class="form-row">
+                     <!--    confirm button-->
+                <div class="confirm_button col-lg-4 mb-3">
+                      <input type="submit" name="Generate" class="btn btn-dark pull-right" value="Generate" >
+                </div>
+            </div>
+            <h6 class="bg-success"><?php display_message(); ?></h6>
+        </form>
+<table class="table table-hover" role="table">
+                <thead role="rowgroup">
+                <tr role="row">
+                    <th scope="col" role="columnheader">First Name</th>
+                    <th scope="col" role="columnheader">Last Name</th>
+                    <th scope="col" role="columnheader">Phone</th>
+                    <th scope="col" role="columnheader">Number of Dose</th>
+                </tr>
+                </thead>
+                <tbody role="rowgroup">
+                <?php 
+               
+                  if (isset($_POST['Generate'])) {
+                      display_nurse_performed(); 
+                  }
+                ?>
+                </tbody>
+            </table>
+</div>
