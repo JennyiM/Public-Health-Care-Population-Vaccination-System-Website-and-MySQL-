9e34016252e3a28fa78011781752eeae756a3700
Guo0606 <74224294+Guo0606@users.noreply.github.com>
2021-11-21 14:50:07 -0500

    Add files via upload

---
+++ resources/templates/report.php
@@ -0,0 +1,30 @@
+<div class="main col-lg-9 col-md-12">
+    <!--person information-->
+    <!--    breadcrumb link-->
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page">Home</li>
+        </ol>
+    </nav>
+    <section>
+        <h3>Welcome!</h3>
+        <div class="row">
+            <div class="item-bar col-lg-3 col-md-6 col-sm-12">
+            </div>
+            <div class="item-bar col-lg-3 col-md-6 col-sm-12">
+                <a class="nav-link" href="index.php?facility_in_city"><i class="fa-solid fa-file-chart-column"></i>
+                <p>Nurse Workload Report</p>
+                </a>
+            </div>
+            <div class="item-bar col-lg-3 col-md-6 col-sm-12">
+                <a class="nav-link" href="index.php?display_nurse_performed"><i class="fa-brands fa-alipay"></i>
+                <p>City Vaccination Report</p>
+                </a>
+            </div>
+            <div class="item-bar col-lg-3 col-md-6 col-sm-12">
+            </div>
+           
+        </div>
+    </section>
+</div>
+</div>
